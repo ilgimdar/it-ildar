99ad08d9ecd82bc8fb352c314ee0bc6a048965f3
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-08 17:13:31 +0300

    version-0.0.9

---
+++ articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'articles/articles.html', {'posts': posts})

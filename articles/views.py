3c888f973de392cf65ed77559c4ee1ecb96c40a1
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-08 20:43:47 +0300

    version-0.0.11

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
@@ -10,5 +11,14 @@ def post_list(request):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'articles/post_edit.html', {'form': form})

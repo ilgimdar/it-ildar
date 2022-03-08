38423cb797c75b0c36203cbf1e71002dc09ac269
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-08 18:39:41 +0300

    version-0.0.10

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('/new/', views.post_new, name='post_new'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'articles/articles.html', {'posts': posts})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'articles/post_edit.html', {'form': form})

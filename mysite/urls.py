eedcf0bb398edde9c4ccb179ab50d9621bcbd127
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-10 17:00:40 +0300

    version-1.0.4

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from . import settings
 from .views import page_not_found
 
 urlpatterns = [
@@ -8,4 +11,6 @@ urlpatterns = [
     path('articles', include('articles.urls'))
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = page_not_found

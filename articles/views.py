f27faabdfc25f1c5a72b25ecf136163fd5f4e19b
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-18 21:17:47 +0300

    version-1.2.3

--- articles/views.py
+++ articles/views.py
@@ -16,6 +16,7 @@ class PostList(ListView):
     model = Post
     template_name = 'articles/articles.html'
     context_object_name = 'posts'
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)

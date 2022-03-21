d1524ff0e2a2f7af399bd86b62b8d79429f0f758
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-21 12:09:16 +0300

    version-1.2.13

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 from articles import views
 
 from . import settings
-from .views import page_not_found
-
+from .views import page_not_found, RegisterUser
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
-    path('login/', views.login, name='login'),
+    path('login/', RegisterUser.as_view(), name='login'),
     path('register/', views.login, name='register'),
+
 ]
 
 if settings.DEBUG:
--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articles.utils import DataMixin
+from mysite.forms import RegisterUserForm
 
 
 def page_not_found(request, exception):
     response = render(request, 'mysite/404.html')
     response.status_code = 404
     return response
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'mysite/register.html'
+    success_url = reverse_lazy('login')
